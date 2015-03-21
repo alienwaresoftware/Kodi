e6651e087a9c07f20250a28f589069a8b7b1cdc7
alienwaresoftware <justin_gray@dell.com>
2015-03-21 14:27:58 -0500

    updated to be visible in kodi

--- script.aw.background.installer/background_installer.py
+++ script.aw.background.installer/background_installer.py
@@ -1,5 +1,6 @@
 import zipfile
 import os
+import xbmc
 background_path = '%s\\Kodi\\Media\\' % os.environ['APPDATA'] 
 dir_path = '%s\\Kodi\\addons\\script.aw.background.installer\\' % os.environ['APPDATA'] 
 file_path = '%sBackgrounds.zip' % dir_path
