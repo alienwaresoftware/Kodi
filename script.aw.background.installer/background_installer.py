a553d27a55557bfcec5cf6fc5877f73237e8b6cc
alienwaresoftware <justin_gray@dell.com>
2015-03-21 14:37:58 -0500

    changed from Kodi to XBMC

--- script.aw.background.installer/background_installer.py
+++ script.aw.background.installer/background_installer.py
@@ -1,8 +1,8 @@
 import zipfile
 import os
 import xbmc
-background_path = '%s\\Kodi\\Media\\' % os.environ['APPDATA'] 
-dir_path = '%s\\Kodi\\addons\\script.aw.background.installer\\' % os.environ['APPDATA'] 
+background_path = '%s\\XBMC\\Media\\' % os.environ['APPDATA'] 
+dir_path = '%s\\XBMC\\addons\\script.aw.background.installer\\' % os.environ['APPDATA'] 
 file_path = '%sBackgrounds.zip' % dir_path
 zfile = zipfile.ZipFile(file_path)
 zfile.extractall(background_path)
