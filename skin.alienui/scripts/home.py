80f8f2c6b8ba78bd0abb8f689adf48a84cb80305
alienwaresoftware <justin_gray@dell.com>
2015-03-31 15:49:51 -0500

    take 2?

--- skin.alienui/scripts/home.py
+++ skin.alienui/scripts/home.py
@@ -1,13 +1,3 @@
-import xbmcgui
 import AlphaUIUtils
-from AlphaUIAutoUpdate import AutoUpdateClass
 
 AlphaUIUtils.CloseLauncher()
-autoUpdateClass = AutoUpdateClass(xbmcgui.Window(xbmcgui.getCurrentWindowId()))
-if(autoUpdateClass.installationOk()):
-    # If the last update was a success, make sure to never show the 'update failed' box
-    ctrl = autoUpdateClass._win.getControl(1001)
-    ctrl.setPosition(-1460, ctrl.getY())                    
-
-autoUpdateClass.showVersionNumber()
-autoUpdateClass.showMessageIfCriticalUpdates()
