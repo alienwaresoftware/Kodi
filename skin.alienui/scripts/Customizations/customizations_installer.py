e62f855f70ce06e34aaacd444a509a2092033790
alienwaresoftware <justin_gray@dell.com>
2015-04-03 10:51:15 -0500

    modified rss

--- skin.alienui/scripts/Customizations/customizations_installer.py
+++ skin.alienui/scripts/Customizations/customizations_installer.py
@@ -1,8 +1,14 @@
-import zipfile, os, xbmc, time
+import zipfile, os, xbmc
 kodi_path = '%s\\XBMC\\' % os.environ['APPDATA'] 
 dir_path = '%s\\XBMC\\addons\\skin.alienui\\scripts\\Customizations\\' % os.environ['APPDATA'] 
-file_path = '%scustomizations.zip' % dir_path
-zfile = zipfile.ZipFile(file_path)
+zip_file_path = '%scustomizations.zip' % dir_path
+advanced_path = '%suserdata\\advancedsettings.xml' % kodi_path
+advance_file_path = '%sadvancedsettings.xml' % dir_path
+zfile = zipfile.ZipFile(zip_file_path)
 zfile.extractall(kodi_path)
+if os.path.exists(advanced_path):
+	pass
+else: 
+	os.rename(advance_file_path, advanced_path)
 alienuiversion = xbmc.getInfoLabel("System.AddonVersion(skin.alienui)")
 xbmc.executebuiltin("Skin.SetString(CustomizationsVersion,"+str(alienuiversion)+")")
