51af0a74046bc6f8b881b33009ad43cedbbe5611
alienwaresoftware <justin_gray@dell.com>
2015-04-02 19:24:31 -0500

    Removed Background installer

--- script.aw.background.installer/background_installer.py
+++
@@ -1,8 +0,0 @@
-import zipfile
-import os
-import xbmc
-background_path = '%s\\XBMC\\Media\\' % os.environ['APPDATA'] 
-dir_path = '%s\\XBMC\\addons\\script.aw.background.installer\\' % os.environ['APPDATA'] 
-file_path = '%sBackgrounds.zip' % dir_path
-zfile = zipfile.ZipFile(file_path)
-zfile.extractall(background_path)
