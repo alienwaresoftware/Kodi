7d2e49fed621555bc0eaaacb6f6268aa29d18049
alienwaresoftware <justin_gray@dell.com>
2015-04-09 16:11:50 -0500

    Changes to home, colors
    Modified home and recently added to better fit on screen. Added silver
    and yellow themes.

--- Build_Textures.bat
+++
@@ -1,8 +0,0 @@
-Title Building Texture Files
-
-C:\Alienware\Kodi\TexturePacker.exe -input C:\Alienware\Kodi\themes\Blue -output C:\Alienware\Kodi\skin.alienui\media\Blue.xbt
-C:\Alienware\Kodi\TexturePacker.exe -input C:\Alienware\Kodi\themes\Red -output C:\Alienware\Kodi\skin.alienui\media\Textures.xbt
-C:\Alienware\Kodi\TexturePacker.exe -input C:\Alienware\Kodi\themes\Green -output C:\Alienware\Kodi\skin.alienui\media\Green.xbt
-C:\Alienware\Kodi\TexturePacker.exe -input C:\Alienware\Kodi\themes\Orange -output C:\Alienware\Kodi\skin.alienui\media\Orange.xbt
-C:\Alienware\Kodi\TexturePacker.exe -input C:\Alienware\Kodi\themes\Purple -output C:\Alienware\Kodi\skin.alienui\media\Purple.xbt
-C:\Alienware\Kodi\TexturePacker.exe -input C:\Alienware\Kodi\themes\Pink -output C:\Alienware\Kodi\skin.alienui\media\Pink.xbt
