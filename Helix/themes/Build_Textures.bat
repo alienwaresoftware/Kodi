22c79d7350a97100d5d98053906082947285f4a7
alienwaresoftware <justin_gray@dell.com>
2015-04-16 14:52:26 -0500

    Updated texture file

--- Helix/themes/Build_Textures.bat
+++ Helix/themes/Build_Textures.bat
@@ -1,7 +1,7 @@
 Title Building Texture Files
 
 C:\Alienware\Kodi\themes\TexturePacker.exe -input C:\Alienware\Kodi\themes\Blue -output C:\Alienware\Kodi\skin.alienui\media\Blue.xbt
-C:\Alienware\Kodi\themes\TexturePacker.exe -input C:\Alienware\Kodi\themes\Red -output C:\Alienware\Kodi\skin.alienui\media\Textures.xbt
+C:\Alienware\Kodi\themes\TexturePacker.exe -input C:\Alienware\Kodi\themes\Red -output C:\Alienware\Kodi\skin.alienui\media\Red.xbt
 C:\Alienware\Kodi\themes\TexturePacker.exe -input C:\Alienware\Kodi\themes\Green -output C:\Alienware\Kodi\skin.alienui\media\Green.xbt
 C:\Alienware\Kodi\themes\TexturePacker.exe -input C:\Alienware\Kodi\themes\Orange -output C:\Alienware\Kodi\skin.alienui\media\Orange.xbt
 C:\Alienware\Kodi\themes\TexturePacker.exe -input C:\Alienware\Kodi\themes\Purple -output C:\Alienware\Kodi\skin.alienui\media\Purple.xbt
