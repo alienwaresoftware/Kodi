9996256b5b9d7ec008f9a988762c983ffedf7885
alienwaresoftware <justin_gray@dell.com>
2015-04-17 14:48:48 -0500

    Updated logo in settings

--- Helix/themes/Build_Textures.bat
+++ Helix/themes/Build_Textures.bat
@@ -1,10 +1,10 @@
 Title Building Texture Files
 
-C:\Alienware\Kodi\themes\TexturePacker.exe -input C:\Alienware\Kodi\themes\Blue -output C:\Alienware\Kodi\skin.alienui\media\Blue.xbt
-C:\Alienware\Kodi\themes\TexturePacker.exe -input C:\Alienware\Kodi\themes\Red -output C:\Alienware\Kodi\skin.alienui\media\Red.xbt
-C:\Alienware\Kodi\themes\TexturePacker.exe -input C:\Alienware\Kodi\themes\Green -output C:\Alienware\Kodi\skin.alienui\media\Green.xbt
-C:\Alienware\Kodi\themes\TexturePacker.exe -input C:\Alienware\Kodi\themes\Orange -output C:\Alienware\Kodi\skin.alienui\media\Orange.xbt
-C:\Alienware\Kodi\themes\TexturePacker.exe -input C:\Alienware\Kodi\themes\Purple -output C:\Alienware\Kodi\skin.alienui\media\Purple.xbt
-C:\Alienware\Kodi\themes\TexturePacker.exe -input C:\Alienware\Kodi\themes\Pink -output C:\Alienware\Kodi\skin.alienui\media\Pink.xbt
-C:\Alienware\Kodi\themes\TexturePacker.exe -input C:\Alienware\Kodi\themes\Yellow -output C:\Alienware\Kodi\skin.alienui\media\Yellow.xbt
-C:\Alienware\Kodi\themes\TexturePacker.exe -input C:\Alienware\Kodi\themes\Silver -output C:\Alienware\Kodi\skin.alienui\media\Silver.xbt
+C:\Alienware\Kodi\Helix\themes\TexturePacker.exe -input C:\Alienware\Kodi\Helix\themes\Blue -output C:\Alienware\Kodi\Helix\skin.alienui\media\Blue.xbt
+C:\Alienware\Kodi\Helix\themes\TexturePacker.exe -input C:\Alienware\Kodi\Helix\themes\Red -output C:\Alienware\Kodi\Helix\skin.alienui\media\Textures.xbt
+C:\Alienware\Kodi\Helix\themes\TexturePacker.exe -input C:\Alienware\Kodi\Helix\themes\Green -output C:\Alienware\Kodi\Helix\skin.alienui\media\Green.xbt
+C:\Alienware\Kodi\Helix\themes\TexturePacker.exe -input C:\Alienware\Kodi\Helix\themes\Orange -output C:\Alienware\Kodi\Helix\skin.alienui\media\Orange.xbt
+C:\Alienware\Kodi\Helix\themes\TexturePacker.exe -input C:\Alienware\Kodi\Helix\themes\Purple -output C:\Alienware\Kodi\Helix\skin.alienui\media\Purple.xbt
+C:\Alienware\Kodi\Helix\themes\TexturePacker.exe -input C:\Alienware\Kodi\Helix\themes\Pink -output C:\Alienware\Kodi\Helix\skin.alienui\media\Pink.xbt
+C:\Alienware\Kodi\Helix\themes\TexturePacker.exe -input C:\Alienware\Kodi\Helix\themes\Yellow -output C:\Alienware\Kodi\Helix\skin.alienui\media\Yellow.xbt
+C:\Alienware\Kodi\Helix\themes\TexturePacker.exe -input C:\Alienware\Kodi\Helix\themes\Silver -output C:\Alienware\Kodi\Helix\skin.alienui\media\Silver.xbt
