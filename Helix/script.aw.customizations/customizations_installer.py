e4cefc6624794c99ceefc6e96a4a7ba854532706
alienwaresoftware <justin_gray@dell.com>
2015-04-17 15:21:49 -0500

    Removed old addons

--- Helix/script.aw.customizations/customizations_installer.py
+++
@@ -1,7 +0,0 @@
-import zipfile
-import os
-kodi_path = '%s\\XBMC\\' % os.environ['APPDATA'] 
-dir_path = '%s\\XBMC\\addons\\script.aw.customizations\\' % os.environ['APPDATA'] 
-file_path = '%scustomizations.zip' % dir_path
-zfile = zipfile.ZipFile(file_path)
-zfile.extractall(kodi_path)
--- Helix/script.aw.tencent.launcher/launch_tencent.py
+++
@@ -1,3 +0,0 @@
-import xbmc
-import subprocess
-subprocess.Popen('"C:\Program Files (x86)\Tencent\Tencent.exe"')
