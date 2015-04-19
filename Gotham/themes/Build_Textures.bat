ab273b1eda3547a536fb55046cc18623e8b11f65
alienwaresoftware <justin_gray@dell.com>
2015-04-19 15:05:49 -0500

    removed themes from main skin
    Update Strings
    
    update settings and home
    
    Updated background images, removed system
    settings menu end point,
    updated alignment issues, made system settings
    menu transparent, changed
    the icon for the selector.

--- Gotham/themes/Build_Textures.bat
+++ Gotham/themes/Build_Textures.bat
@@ -1,10 +1,10 @@
 Title Building Texture Files
 
-C:\Alienware\Kodi\themes\TexturePacker.exe -input C:\Alienware\Kodi\themes\Blue -output C:\Alienware\Kodi\skin.alienui\media\Blue.xbt
-C:\Alienware\Kodi\themes\TexturePacker.exe -input C:\Alienware\Kodi\themes\Red -output C:\Alienware\Kodi\skin.alienui\media\Textures.xbt
-C:\Alienware\Kodi\themes\TexturePacker.exe -input C:\Alienware\Kodi\themes\Green -output C:\Alienware\Kodi\skin.alienui\media\Green.xbt
-C:\Alienware\Kodi\themes\TexturePacker.exe -input C:\Alienware\Kodi\themes\Orange -output C:\Alienware\Kodi\skin.alienui\media\Orange.xbt
-C:\Alienware\Kodi\themes\TexturePacker.exe -input C:\Alienware\Kodi\themes\Purple -output C:\Alienware\Kodi\skin.alienui\media\Purple.xbt
-C:\Alienware\Kodi\themes\TexturePacker.exe -input C:\Alienware\Kodi\themes\Pink -output C:\Alienware\Kodi\skin.alienui\media\Pink.xbt
-C:\Alienware\Kodi\themes\TexturePacker.exe -input C:\Alienware\Kodi\themes\Yellow -output C:\Alienware\Kodi\skin.alienui\media\Yellow.xbt
-C:\Alienware\Kodi\themes\TexturePacker.exe -input C:\Alienware\Kodi\themes\Silver -output C:\Alienware\Kodi\skin.alienui\media\Silver.xbt
+C:\Alienware\Kodi\Gotham\themes\TexturePacker.exe -input C:\Alienware\Kodi\Gotham\themes\Blue -output C:\Alienware\Kodi\Gotham\skin.alienui\media\Blue.xbt
+C:\Alienware\Kodi\Gotham\themes\TexturePacker.exe -input C:\Alienware\Kodi\Gotham\themes\Red -output C:\Alienware\Kodi\Gotham\skin.alienui\media\Textures.xbt
+C:\Alienware\Kodi\Gotham\themes\TexturePacker.exe -input C:\Alienware\Kodi\Gotham\themes\Green -output C:\Alienware\Kodi\Gotham\skin.alienui\media\Green.xbt
+C:\Alienware\Kodi\Gotham\themes\TexturePacker.exe -input C:\Alienware\Kodi\Gotham\themes\Orange -output C:\Alienware\Kodi\Gotham\skin.alienui\media\Orange.xbt
+C:\Alienware\Kodi\Gotham\themes\TexturePacker.exe -input C:\Alienware\Kodi\Gotham\themes\Purple -output C:\Alienware\Kodi\Gotham\skin.alienui\media\Purple.xbt
+C:\Alienware\Kodi\Gotham\themes\TexturePacker.exe -input C:\Alienware\Kodi\Gotham\themes\Pink -output C:\Alienware\Kodi\Gotham\skin.alienui\media\Pink.xbt
+C:\Alienware\Kodi\Gotham\themes\TexturePacker.exe -input C:\Alienware\Kodi\Gotham\themes\Yellow -output C:\Alienware\Kodi\Gotham\skin.alienui\media\Yellow.xbt
+C:\Alienware\Kodi\Gotham\themes\TexturePacker.exe -input C:\Alienware\Kodi\Gotham\themes\Silver -output C:\Alienware\Kodi\Gotham\skin.alienui\media\Silver.xbt
---
+++ Helix/script.aw.backgrounds/install_backgrounds.py
@@ -0,0 +1,8 @@
+import zipfile, os, xbmc
+kodi_path = '%s\\XBMC\\media\\' % os.environ['APPDATA'] 
+dir_path = '%s\\XBMC\\addons\\script.aw.backgrounds\\' % os.environ['APPDATA'] 
+zip_file_path = '%sthemes.zip' % dir_path
+zfile = zipfile.ZipFile(zip_file_path)
+zfile.extractall(kodi_path)
+backgroundsversion = xbmc.getInfoLabel("System.AddonVersion(script.aw.backgrounds)")
+xbmc.executebuiltin("Skin.SetString(BackgroundsVersion,"+str(backgroundsversion)+")")
