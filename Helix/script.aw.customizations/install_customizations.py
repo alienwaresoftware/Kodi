ac363eca36e62e1d97cd6ddcc13bceb5648cb9e6
alienwaresoftware <justin_gray@dell.com>
2015-04-19 21:14:40 -0500

    Finished updating customizations

--- Helix/script.aw.customizations/install_customizations.py
+++ Helix/script.aw.customizations/install_customizations.py
@@ -4,5 +4,5 @@ dir_path = '%s\\Kodi\\addons\\script.aw.customizations\\' % os.environ['APPDATA'
 zip_file_path = '%scustomizations.zip' % dir_path
 zfile = zipfile.ZipFile(zip_file_path)
 zfile.extractall(kodi_path)
-customizationsversion = xbmc.getInfoLabel("System.AddonVersion(script.aw.customizations)")
-xbmc.executebuiltin("Skin.SetString(CustomizationsVersion,"+str(customizationsversion)+")")
+CustomizationsVersion = xbmc.getInfoLabel("System.AddonVersion(script.aw.customizations)")
+xbmc.executebuiltin("Skin.SetString(CustomizationsVersion,"+str(CustomizationsVersion)+")")
