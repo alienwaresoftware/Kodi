b1ba780cac0307fb7279ae92d5233642210e558c
alienwaresoftware <justin_gray@dell.com>
2015-04-23 11:20:33 -0500

    Services
    Made backgrounds, customization, and AlienUI Launcher services that run
    at startup. Updated Home.xml accordingly.

--- Helix/script.aw.backgrounds/install_backgrounds.py
+++ Helix/script.aw.backgrounds/install_backgrounds.py
@@ -3,6 +3,9 @@ kodi_path = '%s\\Kodi\\media\\' % os.environ['APPDATA']
 dir_path = '%s\\Kodi\\addons\\script.aw.backgrounds\\' % os.environ['APPDATA'] 
 zip_file_path = '%sBackgrounds.zip' % dir_path
 zfile = zipfile.ZipFile(zip_file_path)
-zfile.extractall(kodi_path)
 backgroundsversion = xbmc.getInfoLabel("System.AddonVersion(script.aw.backgrounds)")
-xbmc.executebuiltin("Skin.SetString(BackgroundsVersion,"+str(backgroundsversion)+")")
+backgroundsversionNow = xbmc.getInfoLabel("Skin.String(backgroundsversion)")
+if backgroundsversion > backgroundsversionNow:
+	zfile.extractall(kodi_path)
+	xbmc.executebuiltin("Skin.SetString(BackgroundsVersion,"+str(backgroundsversion)+")")
+else: pass
--- Helix/script.aw.customizations/install_customizations.py
+++ Helix/script.aw.customizations/install_customizations.py
@@ -3,6 +3,9 @@ kodi_path = '%s\\Kodi\\' % os.environ['APPDATA']
 dir_path = '%s\\Kodi\\addons\\script.aw.customizations\\' % os.environ['APPDATA'] 
 zip_file_path = '%scustomizations.zip' % dir_path
 zfile = zipfile.ZipFile(zip_file_path)
-zfile.extractall(kodi_path)
 CustomizationsVersion = xbmc.getInfoLabel("System.AddonVersion(script.aw.customizations)")
-xbmc.executebuiltin("Skin.SetString(CustomizationsVersion,"+str(CustomizationsVersion)+")")
+CustomizationsVersionNow = xbmc.getInfoLabel("Skin.String(CustomizationsVersion)")
+if CustomizationsVersion > CustomizationsVersionNow:
+	zfile.extractall(kodi_path)
+	xbmc.executebuiltin("Skin.SetString(CustomizationsVersion,"+str(CustomizationsVersion)+")")
+else: pass
