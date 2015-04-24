15e1bd7429fd95990d37afc0fef6fdcc17d32e10
alienwaresoftware <justin_gray@dell.com>
2015-04-24 11:19:16 -0500

    Fixed backgrounds script

--- Helix/service.aw.backgrounds/install_backgrounds.py
+++ Helix/service.aw.backgrounds/install_backgrounds.py
@@ -3,9 +3,12 @@ kodi_path = '%s\\Kodi\\media\\' % os.environ['APPDATA']
 dir_path = '%s\\Kodi\\addons\\service.aw.backgrounds\\' % os.environ['APPDATA'] 
 zip_file_path = '%sBackgrounds.zip' % dir_path
 zfile = zipfile.ZipFile(zip_file_path)
-backgroundsversion = xbmc.getInfoLabel("System.AddonVersion(service.aw.backgrounds)")
-backgroundsversionNow = xbmc.getInfoLabel("Skin.String(backgroundsversion)")
-if backgroundsversion > backgroundsversionNow:
+BackgroundsVersion = xbmc.getInfoLabel("System.AddonVersion(service.aw.backgrounds)")
+BackgroundsVersionNow = xbmc.getInfoLabel("Skin.String(BackgroundsVersion)")
+if BackgroundsVersion > BackgroundsVersionNow:
 	zfile.extractall(kodi_path)
-	xbmc.executebuiltin("Skin.SetString(BackgroundsVersion,"+str(backgroundsversion)+")")
-else: pass
+	xbmc.executebuiltin("Skin.SetString(BackgroundsVersion,"+str(BackgroundsVersion)+")")
+	print "Backgrounds Updated"
+else: 
+	print "Backgrounds Failed"
+	pass
