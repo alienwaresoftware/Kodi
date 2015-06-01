42b4526c733861e88089f550a33ab4ccc9bb9988
indidevelopers <mehul_patel@indilogic.com>
2015-06-01 06:23:06 -0500

    plugin.program.aw.gamelauncher: updated to version 0.0.3
    1) added Add to Favorites and Remove from Favorites features
    2) Display all the installed steam games.
    
    repository.helix.alienware: updated to version 1.1.3
    
    1) Update version as Addons updated
    
    script.service.aw.devicesettings: updated to version 0.11.0
    
    1) Added fanart.jpg
    
    service.aw.backgrounds: updated to version 0.11.0
    
    1) Added fanart.jpg
    
    service.aw.customizations: updated to version 0.13.0
    
    1) Added fanart.jpg
    
    service.aw.launcher: updated to version 0.0.3
    
    1) Added fanart.jpg
    
    service.aw.themes: updated to version 0.12.0
    
    1) Added fanart.jpg
    
    skin.hive: updated to version 0.13.0
    
    1) Added fanart.jpg
    2) Fixed incompatibility issue

--- Helix/plugin.program.aw.gamelauncher/addon.py
+++ Helix/plugin.program.aw.gamelauncher/addon.py
@@ -10,10 +10,19 @@ from _winreg import *
 import json
 import subprocess
 import AlphaUIUtils
+from xml.etree import ElementTree as ET
+from xml.dom import minidom
+import urllib2
+import traceback
+import datetime
+import time
 
 from resources.lib.acf import *
 import resources.lib.common as common
 from resources.lib.common import log
+from resources.lib.common import localize
+from xml.dom.minidom import parse
+from xml.dom.minidom import Document
 
 __addon__        = common.__addon__
 __addonid__ = common.__addonid__
@@ -22,19 +31,14 @@ __addonname__    = common.__addonname__
 __addonpath__    = common.__addonpath__
 __icon__         = common.__icon__
 
-print __addon__
-print __addonversion__
-print __addonid__
-print __addonpath__
-print __icon__
-
 # Addon paths definition
 PLUGIN_DATA_PATH = xbmc.translatePath(os.path.join("special://profile/addon_data",__addonid__))
 BASE_PATH = xbmc.translatePath(os.path.join("special://","profile"))
 HOME_PATH = xbmc.translatePath(os.path.join("special://","home"))
-FAVOURITES_PATH = xbmc.translatePath( 'special://profile/favourites.xml' )
+FAVOURITES_PATH = xbmc.translatePath('special://profile/favourites.xml')
 ADDONS_PATH = xbmc.translatePath(os.path.join(HOME_PATH,"addons"))
 CURRENT_ADDON_PATH = xbmc.translatePath(os.path.join(ADDONS_PATH,__addonid__))
+CURRENT_ADDON_LIB_PATH =  xbmc.translatePath(os.path.join(ADDONS_PATH,__addonid__,"resources","lib"))
 GAMES_DB_PATH = os.path.join(PLUGIN_DATA_PATH,"games.db")
 DEFAULT_THUMB_PATH = os.path.join(PLUGIN_DATA_PATH,"thumbs")
 DEFAULT_FANART_PATH = os.path.join(PLUGIN_DATA_PATH,"fanarts")
@@ -44,7 +48,6 @@ log(CURRENT_ADDON_PATH)
 # Addon paths creation
 if not os.path.exists(DEFAULT_THUMB_PATH): os.makedirs(DEFAULT_THUMB_PATH)
 if not os.path.exists(DEFAULT_FANART_PATH): os.makedirs(DEFAULT_FANART_PATH)
-
 
 class Game(object):
     def __init__(self, gameId, title, type, thumbImage, fanartImage):
@@ -75,7 +78,6 @@ def getSteamPath():
 
         for arch_key in arch_keys:
 
-            print "reading"
             key = _winreg.OpenKey(_winreg.HKEY_LOCAL_MACHINE, r"SOFTWARE\Valve\Steam", 0, _winreg.KEY_READ | arch_key)
             try:
                 steamPath = _winreg.QueryValueEx(key, 'InstallPath')[0]
@@ -89,21 +91,76 @@ def getSteamPath():
         pass
     return steamPath
 
-def downloadGameImage(gameId):
+def downloadFile(url, filePath):
+    opener = urllib2.build_opener()
+    opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+    resource = opener.open(url, timeout=30)
+    file = open(filePath,"wb")
+    file.write(resource.read())
+    file.close()
+
+def downloadGameImage(gameId, gameName):
+    log('Downloading Steam game images for {0} -> {1}'.format(gameId, gameName))
+    
     files = []
-    thumbFilePath = os.path.join(DEFAULT_THUMB_PATH,'thumb_{0}.jpg'.format(gameId))
+    thumbFilePath = os.path.join(DEFAULT_THUMB_PATH,'thumb_{0}_{1}.jpg'.format(gameName, int((time.mktime(datetime.datetime.now().timetuple())))))
+    fanartPath = os.path.join(DEFAULT_FANART_PATH,'fanart_{0}_{1}.jpg'.format(gameName, int((time.mktime(datetime.datetime.now().timetuple())))))
+
+    for fileName in glob(os.path.join(DEFAULT_THUMB_PATH,'thumb_{0}*.*'.format(gameName))):
+        os.remove(fileName)
+
+    for fileName in glob(os.path.join(DEFAULT_FANART_PATH,'fanart_{0}*.*'.format(gameName))):
+        os.remove(fileName)
+
+    isFanartFound = False
+    isThumbImageFound = False
+
+    try:
+        gameUrlQuery = {'exactname':gameName.encode('utf8')}
+
+        requestURL = 'http://thegamesdb.net/api/GetGame.php?' + urllib.urlencode(gameUrlQuery)
+    
+        opener = urllib2.build_opener()
+        opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+
+        xmlResponse = opener.open(requestURL, timeout=30)
+
+        strXml = xmlResponse.read()
+
+        root = ET.fromstring(strXml)
+        baseImgUrl = root[0].text
+        fanartNodes = root.findall('.//Game/Images/fanart/original')
+        fanartImg = fanartNodes[0].text
+
+        downloadFile(baseImgUrl + fanartImg, fanartPath)
+        isFanartFound = True
+    except:
+        pass
+
+    try:
+        response = urllib.urlopen('http://store.steampowered.com/api/appdetails?appids={0}'.format(gameId))
+        gameJason = json.loads(response.read())
+        response.close()
+
+        if(not isThumbImageFound):
+            downloadFile(gameJason[gameId]['data']['header_image'], thumbFilePath)
+            isThumbImageFound = True
+
+        if(not isFanartFound):
+            downloadFile(gameJason[gameId]['data']['screenshots'][0]['path_full'], fanartPath)
+            isFanartFound = True    
+
+    except:
+        pass    
+
+    if(not isThumbImageFound):
+        thumbFilePath = os.path.join(__addonpath__,'resources','skins','Default','media','alienware','steam.png')
+    if(not isFanartFound):
+        fanartPath = os.path.join(__addonpath__,'resources','skins','Default','media','alienware','fanart_steam.png')
+
     files.append(thumbFilePath)
-    fanartPath = os.path.join(DEFAULT_FANART_PATH,'fanart_{0}.jpg'.format(gameId))
     files.append(fanartPath)
-
-    if (os.path.isfile(thumbFilePath) and os.path.isfile(fanartPath)):
-         return files
-
-    response = urllib.urlopen('http://store.steampowered.com/api/appdetails?appids={0}'.format(gameId))
-    gameJason = json.loads(response.read())
-    response.close()
-    urllib.urlretrieve(gameJason[gameId]['data']['header_image'], thumbFilePath)
-    urllib.urlretrieve(gameJason[gameId]['data']['screenshots'][0]['path_full'], fanartPath)
+    
     return files
 
 def findGames():
@@ -114,30 +171,30 @@ def findGames():
 
     files = glob('{0}/*.acf'.format(steamAppPath))
     numFiles = len(files)
-    log(numFiles)
     counter = 0
 
     pDialog = xbmcgui.DialogProgress()
-    pDialog.create("Game Finder", "Finding Games....")
-    pDialog.update(0, "Finding Games....")
+    pDialog.create(__addonname__, localize(33003))
+    pDialog.update(0, localize(33003))
 
     for filePath in glob('{0}/*.acf'.format(steamAppPath)):
         gameDict = parse_acf(filePath)
-        log(gameDict)
         if (gameDict['AppState']['BytesToDownload'] != '0' and gameDict['AppState']['BytesToDownload'] == gameDict['AppState']['BytesDownloaded']):
-            gameFiles = downloadGameImage(gameDict['AppState']['appid'])
+            gameFiles = downloadGameImage(gameDict['AppState']['appid'],gameDict['AppState']['name'])
             games[gameDict['AppState']['appid']] = {}
             games[gameDict['AppState']['appid']]['name'] = gameDict['AppState']['name']
             games[gameDict['AppState']['appid']]['type'] = 1
             games[gameDict['AppState']['appid']]['thumbImage'] = gameFiles[0]
             games[gameDict['AppState']['appid']]['fanartImage'] = gameFiles[1]
             counter = counter + 1
-            pDialog.update(int((float(counter)/float(numFiles))*100), "Finding Games")
+            pDialog.update(int((float(counter)/float(numFiles))*100), localize(33003))
 
     gamesDbObj = open(GAMES_DB_PATH, 'w')
     gamesDbObj.write(json.dumps(games))
     gamesDbObj.close()
     pDialog.close()
+    dialog = xbmcgui.Dialog()
+    ok = dialog.ok(__addonname__, str(numFiles) + ' ' + localize(33004))
     
 def getGames():
     games = []
@@ -150,43 +207,119 @@ def getGames():
     gamesDbfile.close()
 
     for game in gamesJson:
-        log(game)
         games.append(Game(game, gamesJson[game]['name'], gamesJson[game]['type'], gamesJson[game]['thumbImage'], gamesJson[game]['fanartImage']))
 
     return games    
 
-mode = args.get('mode', None)
-log("Starting")
+def isInFavorites(game):
+    if os.path.isfile( FAVOURITES_PATH ):
+        fav_xml = parse( FAVOURITES_PATH )
+        fav_doc = fav_xml.documentElement.getElementsByTagName( 'favourite' )
+        for count, favourite in enumerate(fav_doc):
+            if (favourite.attributes['name'].nodeValue == game.title and favourite.firstChild.data == getGameLaunchUrlAction(game)):
+                return True
+    else:
+        return False
 
+def getGameLaunchUrlAction(game):
+    return 'PlayMedia("{0}")'.format(build_url({'mode': 'folder', 'action': 'launchgame', 'gameid': game.gameId, 'gametype': game.type}))
+
+def addContextMenu(game,listItem):
+    #ActivateWindow(10001,&quot;plugin://plugin.program.aw.gamelauncher/?action=addtofavorite&amp;mode=folder&quot;,return)
+
+    menuTuple = None
+
+    if (isInFavorites(game)):
+       menuTuple = (xbmc.getLocalizedString(14077), 'XBMC.Container.Refresh(plugin://plugin.program.aw.gamelauncher/?action=games&amp;menuaction=removefromfavorites&amp;mode=folder&amp;gameid={0})'.format(game.gameId),)
+    else:
+       menuTuple = (xbmc.getLocalizedString(14076), 'XBMC.Container.Refresh(plugin://plugin.program.aw.gamelauncher/?action=games&amp;menuaction=addtofavorites&amp;mode=folder&amp;gameid={0})'.format(game.gameId),)
+
+    listItem.addContextMenuItems([menuTuple,
+                                    (localize(33005), 'XBMC.Container.Refresh(plugin://plugin.program.aw.gamelauncher/?action=games&amp;menuaction=edittitle&amp;mode=folder&amp;gameid={0})'.format(game.gameId),),
+                                    (localize(33006), 'XBMC.Container.Refresh(plugin://plugin.program.aw.gamelauncher/?action=games&amp;menuaction=editpath&amp;mode=folder&amp;gameid={0})'.format(game.gameId),),
+                                    (localize(33007), 'XBMC.Container.Refresh(plugin://plugin.program.aw.gamelauncher/?action=games&amp;menuaction=editicon&amp;mode=folder&amp;gameid={0})'.format(game.gameId),),
+                                    (localize(33008), 'XBMC.Container.Refresh(plugin://plugin.program.aw.gamelauncher/?action=games&amp;menuaction=editfanart&amp;mode=folder&amp;gameid={0})'.format(game.gameId),),],True)
+
+def addToFavorite(game):
+    doc = None
+    root = None
+
+    log('add to favorite ' + game.title)
+
+    if (not os.path.isfile(FAVOURITES_PATH)):            
+        doc = Document()
+        root = doc.createElement("favourites")
+        doc.appendChild(root)
+    else:
+        doc = parse( FAVOURITES_PATH )
+        root = doc.documentElement
+
+    favNode = doc.createElement("favourite")
+    root.appendChild(favNode)
+
+    favNode.setAttribute( "name", game.title)
+    favNode.setAttribute( "thumb", game.thumbImage)
+
+    url = getGameLaunchUrlAction(game)
+
+    textNode = doc.createTextNode(url)
+    favNode.appendChild(textNode)
+ 
+    doc.writexml(open(FAVOURITES_PATH, 'w'))
+ 
+    doc.unlink()
+
+def removeFromFavorite(game):
+    if os.path.isfile( FAVOURITES_PATH ):
+        doc = parse( FAVOURITES_PATH )
+        fav_doc = doc.documentElement.getElementsByTagName('favourite')
+        for count, favourite in enumerate(fav_doc):
+            if (favourite.attributes['name'].nodeValue == game.title and favourite.firstChild.data == getGameLaunchUrlAction(game)):
+                doc.documentElement.removeChild(favourite)
+                break;
+
+    doc.writexml(open(FAVOURITES_PATH, 'w'))
+ 
+    doc.unlink()
+
+
+mode = args.get('mode', None)
+log(args)
 if mode is None:
     imageFilePath = os.path.join(CURRENT_ADDON_PATH, 'resources','skins','Default','media','alienware')
 
     url = build_url({'mode': 'folder', 'action': 'games'})
     #print "url -> {0}".format(url)
-    li = xbmcgui.ListItem('Games', iconImage=os.path.join(imageFilePath, 'games.png'))
+    li = xbmcgui.ListItem(localize(33000), iconImage=os.path.join(imageFilePath, 'games.png'))
     #log(os.path.join(imageFilePath, 'games.png'))
-    li.setProperty('Fanart_Image', os.path.join(CURRENT_ADDON_PATH, 'fanart.jpg'))
+    #li.setProperty('Fanart_Image', os.path.join(CURRENT_ADDON_PATH, 'fanart.jpg'))
     #log(os.path.join(CURRENT_ADDON_PATH, 'fanart.jpg'))
     xbmcplugin.addDirectoryItem(handle=addon_handle, url=url,
                                 listitem=li, isFolder=True)
 
     url = build_url({'mode': 'folder', 'action': 'findgames'})
-    li = xbmcgui.ListItem('Find Games', iconImage=os.path.join(imageFilePath, 'findgames.png'))
-    li.setProperty('Fanart_Image', os.path.join(CURRENT_ADDON_PATH, 'fanart.jpg'))
+    li = xbmcgui.ListItem(localize(33001), iconImage=os.path.join(imageFilePath, 'findgames.png'))
+    #li.setProperty('Fanart_Image', os.path.join(CURRENT_ADDON_PATH, 'fanart.jpg'))
     xbmcplugin.addDirectoryItem(handle=addon_handle, url=url,
                                 listitem=li, isFolder=True)
     
     url = build_url({'mode': 'folder', 'action': 'steam'})
-    li = xbmcgui.ListItem('Launch Steam', iconImage=os.path.join(imageFilePath, 'steam.png'))
-    li.setProperty('Fanart_Image', os.path.join(CURRENT_ADDON_PATH, 'fanart.jpg'))
+    li = xbmcgui.ListItem(localize(33002), iconImage=os.path.join(imageFilePath, 'steam.png'))
+    #li.setProperty('Fanart_Image', os.path.join(CURRENT_ADDON_PATH, 'fanart.jpg'))
     xbmcplugin.addDirectoryItem(handle=addon_handle, url=url,
                                 listitem=li, isFolder=True)
 
-    xbmcplugin.endOfDirectory(addon_handle)
+    xbmcplugin.endOfDirectory(addon_handle, cacheToDisc=False)
 
-elif (mode[0] == 'folder'):
+elif (mode[0] == 'folder'):    
     actionName = args['action'][0]
-    print "actionName -> {0}".format(actionName)
+
+    menuActionName = ""
+    try:
+        if (args['menuaction']):
+            menuActionName = args['menuaction'][0]
+    except:
+        pass
 
     if (actionName == 'steam'):
         try:
@@ -197,14 +330,30 @@ elif (mode[0] == 'folder'):
         findGames()
     elif (actionName == 'games'):
         games = getGames()
+
+        if (menuActionName == 'addtofavorites'):
+            log('add to favorite ' + args['gameid'][0])
+
+            if (games):
+                for game in games:
+                    if (game.gameId == args['gameid'][0]):
+                        log('game found')
+                        addToFavorite(game)
+                        break;
+        elif (menuActionName == 'removefromfavorites'):
+            if (games):
+                for game in games:
+                    if (game.gameId == args['gameid'][0]):
+                        removeFromFavorite(game)
+                        break;
         if (games):
             for game in games:
                 url = build_url({'mode': 'folder', 'action': 'launchgame', 'gameid': game.gameId, 'gametype': game.type})
-                print url
                 li = xbmcgui.ListItem(game.title, iconImage=game.thumbImage)
                 li.setProperty('Fanart_Image', game.fanartImage)
+                addContextMenu(game,li)
                 xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
-            xbmcplugin.endOfDirectory(addon_handle)    
+            xbmcplugin.endOfDirectory(addon_handle, cacheToDisc=False)    
     elif (actionName == 'launchgame'):
         gameId = args['gameid'][0]
         gameType = args['gametype'][0]
--- Helix/plugin.program.aw.gamelauncher/resources/lib/game.py
+++
@@ -1,7 +0,0 @@
-class Game(object):
-    def __init__(self, gameId, title, type, thumbImage, fanartImage):
-        self.gameId = gameId
-        self.title = title
-        self.type = type
-        self.thumbImage = thumbImage
-        self.fanartImage = fanartImage
