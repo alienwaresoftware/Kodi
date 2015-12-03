ecab055d3a576953da617af56debc79ddb81e2eb
Justin Gray <justinanthonygray@gmail.com>
2015-12-03 12:28:56 -0600

    remove skin

--- Development/skin.alphaui/scripts/Help_Scripts/launch_wiki_basic_controls.py
+++
@@ -1,3 +0,0 @@
-import xbmc
-import subprocess
-subprocess.Popen('cmd /c start http://kodi.wiki/view/Home_screen_and_basic_controls')
--- Development/skin.alphaui/scripts/Help_Scripts/launch_wiki_music.py
+++
@@ -1,3 +0,0 @@
-import xbmc
-import subprocess
-subprocess.Popen('cmd /c start http://kodi.wiki/view/Adding_music_to_the_library')
